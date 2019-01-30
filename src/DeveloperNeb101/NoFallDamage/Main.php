849b8d2bcf0054bd0b34080acca692e49da98c56
RaccoonUndercover <46036793+RaccoonUndercover@users.noreply.github.com>
2019-01-30 15:56:54 -0800

    Fix Main

--- src/DeveloperNeb101/NoFallDamage/Main.php
+++ src/DeveloperNeb101/NoFallDamage/Main.php
@@ -1,16 +1,20 @@
 <?php
 namespace DeveloperNeb101\NoFallDamage;
-use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\plugin\PluginBase;
 class Main extends PluginBase implements Listener{
   public function onEnable(){
      $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
-  public function e_damage(EntityDamageEvent $event){
-    if($event->getCause()===EntityDamageEvent::CAUSE_FALL)
+ public function e_damage(EntityDamageByEntityEvent $event, EntityDamageEvent $ev) {
+    if($ev === true) {
+      return true;
+  } else if($event2->getCause()===EntityDamageEvent::CAUSE_FALL) {
       $event->setCancelled();
+  } else {
+    return true;
   }
+ }
 }
